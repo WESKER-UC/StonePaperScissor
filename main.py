096396a22e0f40bd9c2655e6dc24eb497d66b3c5
JAMES <jjeshurun845@gmail.com>
2026-01-12 20:27:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request,session, url_for
 from random import randint as rd
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY']=os.getenv('SECRET_KEY')
 def cvu(uch)->int:
     cch = rd(1,3)
     if uch == cch : return -1,cch
@@ -13,11 +16,14 @@ def cvu(uch)->int:
 
 @app.route("/",methods=['GET'])
 def hello_world():
-    return render_template('SPC.html')
+    session['userscore']=0
+    return redirect(url_for('scp'))
 
 @app.route('/scp',methods = ['POST','GET'])
 def scp():
     if request.method=='POST':
+        
+        
         uchval = request.form.get('radioDefault')
         if not uchval:
             return render_template('SPC.html', ans=4)
@@ -25,7 +31,21 @@ def scp():
         chl=['stone','paper','scissor']
         uch = ch[uchval]
         ans = cvu(uch)
-        return render_template('SPC.html',ans=ans[0],cch=chl[ans[1]-1],uch=chl[uch-1])
+        if session.get('count')==0:
+                session['count']=1
+                session['userscore']=0
+        if ans[0] == 1 or ans[0] == -1:
+            if session.get('count')==0:
+                session['count']=1
+                session['userscore']=0
+                
+            else:
+                session['userscore']=session.get('userscore')+10
+                
+        else:
+            session['userscore']=session.get('userscore')-5
+        
+        return render_template('SPC.html',ans=ans[0],cch=chl[ans[1]-1],uch=chl[uch-1], score = session.get('userscore'))
     
     return render_template('SPC.html',ans=4)
 if __name__ == '__main__':
